fca8e34b909d7ca85f0bcbfa5fcd7ef145e767e6
Wayne Dahlberg <waynefj40@gmail.com>
2023-12-07 22:12:29 -0700

    initial commit

---
+++ 100DaysOfSwiftUI/100DaysOfSwiftUI/_00DaysOfSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  _00DaysOfSwiftUIApp.swift
+//  100DaysOfSwiftUI
+//
+//  Created by Wayne Dahlberg on 12/7/23.
+//
+
+import SwiftUI
+
+@main
+struct _00DaysOfSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
